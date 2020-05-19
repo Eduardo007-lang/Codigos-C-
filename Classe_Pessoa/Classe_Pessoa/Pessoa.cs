fc0e582c266c5cf58b0d674da8129072ca521f07
Eduardo007-lang <53060876+Eduardo007-lang@users.noreply.github.com>
2020-05-19 16:48:30 -0300

    Add files via upload

---
+++ Classe_Pessoa/Classe_Pessoa/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classe_Pessoa
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+    }
+}
---
+++ Classe_Pessoa_2/Classe_Pessoa_2/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classe_Pessoa_2
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+
+    }
+}
