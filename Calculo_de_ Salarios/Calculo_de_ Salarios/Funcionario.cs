c3e60330ba9463f6457a1e3a7024644f6606f766
Eduardo007-lang <53060876+Eduardo007-lang@users.noreply.github.com>
2020-06-30 12:44:35 -0300

    Add files via upload

---
+++ Calculo_de_ Salarios/Calculo_de_ Salarios/Funcionario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Calculo_de__Salarios
+{
+    class Funcionario
+    {   
+        //Atributos Privados
+        private string Nome;
+        //propriedades autoimplementadas
+        public double SalarioBruto { get; set; }
+        public double Imposto { get;  set; }
+
+        //Construtores
+        public Funcionario() { }
+
+        public Funcionario(string nome, double salariobruto, double imposto ) 
+        {
+            Nome = nome;
+            SalarioBruto = salariobruto;
+            Imposto = imposto;
+            
+        }
+        //Propriedades customizadas 
+        public string nome 
+        {
+            get { return Nome; }
+            set {if(value != null ) 
+                Nome = value; }        
+        }
+        //Outros metodos da Classe
+        public double SalarioLiquido() 
+        {
+            return SalarioBruto - Imposto;
+        
+        }
+        public double AumentarSalario(double porcentagem) 
+        {
+            return SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.00);
+        
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Calculo_de_ Salarios/Calculo_de_ Salarios/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculo_de__Salarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+           func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário:" + func );
+            Console.WriteLine();
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
