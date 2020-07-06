3aa99beb8b056d89fce276bb97a137a6874624f2
Eduardo007-lang <53060876+Eduardo007-lang@users.noreply.github.com>
2020-07-06 15:15:13 -0300

    Add files via upload

---
+++ Course/Course_2/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Course_2
+{
+    class ContaBancaria
+    {
+        //Atributos privados
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo  { get; private set; }
+
+        //Construtores de 2 argumentos
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+           
+        }
+        //Construtores de 3 argumentos utilizando o "this"
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) 
+        {
+            deposito(depositoInicial);
+        } 
+
+        //Outros metodos (Deposito, Saque e override)
+        public void deposito(double quantia) 
+        {
+            Saldo += quantia;
+        }
+        public void Saque(double quantia) 
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero 
+                + ", Titular: " 
+                + Titular 
+                + ", Saldo: $" 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Course/Course_2/Produto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_2
+{
+    class Produto
+    {   
+        //ATRIBUTOS PRIVADOS
+        private string _nome;
+        //PROPIEDADES AUTOIMPLEMENTADA
+        //Auto Propeties 
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        //CONSTRUTORES
+        //Construtor Padrão
+        public Produto(){ }
+        //Construtor de 3 Argumentos
+        public Produto(string nome, double preco, int quantidade) 
+        {
+            _nome = nome;
+           Preco = preco;
+            Quantidade = quantidade;
+        
+        }
+        //PROPRIEDADES CUSTOMIZADOS
+        //Propeties
+        public string Nome 
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+
+                    _nome = value;
+                }
+            }
+        
+        }
+        //OUTROS METODOS DA CLASSE
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
