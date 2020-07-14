7abf9b95240e948c315ff5b8c0faaa334f14e8c7
Eduardo007-lang <53060876+Eduardo007-lang@users.noreply.github.com>
2020-07-14 19:36:09 -0300

    Add files via upload

---
+++ Tratamento _De_Erro/Tratamento _De_Erro/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Tratamento__De_Erro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntCalcular = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblsoma = new System.Windows.Forms.Label();
+            this.lblIgual = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bntCalcular
+            // 
+            this.bntCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntCalcular.Location = new System.Drawing.Point(118, 165);
+            this.bntCalcular.Name = "bntCalcular";
+            this.bntCalcular.Size = new System.Drawing.Size(133, 43);
+            this.bntCalcular.TabIndex = 0;
+            this.bntCalcular.Text = "Calcular";
+            this.bntCalcular.UseVisualStyleBackColor = true;
+            this.bntCalcular.Click += new System.EventHandler(this.BntCalcular_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(64, 112);
+            this.txtNum1.Multiline = true;
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(53, 30);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(154, 112);
+            this.txtNum2.Multiline = true;
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(53, 30);
+            this.txtNum2.TabIndex = 2;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(244, 112);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(53, 30);
+            this.txtResultado.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Soma ";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblsoma
+            // 
+            this.lblsoma.AutoSize = true;
+            this.lblsoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsoma.Location = new System.Drawing.Point(123, 117);
+            this.lblsoma.Name = "lblsoma";
+            this.lblsoma.Size = new System.Drawing.Size(25, 25);
+            this.lblsoma.TabIndex = 5;
+            this.lblsoma.Text = "+";
+            // 
+            // lblIgual
+            // 
+            this.lblIgual.AutoSize = true;
+            this.lblIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIgual.Location = new System.Drawing.Point(213, 115);
+            this.lblIgual.Name = "lblIgual";
+            this.lblIgual.Size = new System.Drawing.Size(25, 25);
+            this.lblIgual.TabIndex = 6;
+            this.lblIgual.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 270);
+            this.Controls.Add(this.lblIgual);
+            this.Controls.Add(this.lblsoma);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.bntCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntCalcular;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblsoma;
+        private System.Windows.Forms.Label lblIgual;
+    }
+}
+
---
+++ Tratamento _De_Erro/Tratamento _De_Erro/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tratamento__De_Erro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BntCalcular_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+            try
+            {
+                num1 = int.Parse(txtNum1.Text);
+                num2 = int.Parse(txtNum2.Text);
+                resultado = num1 + num2;
+                txtResultado.Text = resultado.ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Digite Somente Valores numericos", "Catch");
+
+            }
+            finally
+            {
+                MessageBox.Show("Aperte OK, para sair.");
+                Close();
+            }
+        }
+    }
+}
