9d89414c495d218c8466e87b604728994aacd66f
Eduardo007-lang <53060876+Eduardo007-lang@users.noreply.github.com>
2020-07-27 17:38:20 -0300

    Add files via upload

---
+++ Couse2/Couse2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Transactions;
+
+namespace Couse2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("How many rooms will be rented: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Rent[] vet = new Rent[10];
+
+            for (int i = 1; i <= n; i++) 
+            {
+                Console.WriteLine("Rent #{0}: ", i);
+                Console.Write("Name: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Bedroom: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Rent(nome, email);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Occupied rooms: ");
+            for (int i = 0; i < 10; i++) 
+            { 
+             if(vet[i] != null)
+                    Console.WriteLine(i + ": " + vet[i]);
+
+
+            }
+
+
+      
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Couse2/Couse2/Rent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Couse2
+{
+    class Rent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Rent(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
