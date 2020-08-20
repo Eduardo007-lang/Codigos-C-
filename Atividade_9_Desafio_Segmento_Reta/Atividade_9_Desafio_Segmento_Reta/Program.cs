24a038cd1b15e8488f9a8302adfdaab3c896e7d8
Eduardo007-lang <53060876+Eduardo007-lang@users.noreply.github.com>
2020-08-20 16:37:56 -0300

    Add files via upload

---
+++ Atividade_9_Desafio_Segmento_Reta/Atividade_9_Desafio_Segmento_Reta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atividade_9_Desafio_Segmento_Reta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro tamanho da reta:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo tamanho da reta:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o terceiro tamanho da reta:");
+            int c = int.Parse(Console.ReadLine());
+
+            bool triangulo = (a < b + c) && (b < c + a) && (c < a + b);
+
+            if (triangulo == true)
+            {
+
+                Console.WriteLine("Forma um triangulo");
+            }
+            else {
+
+                Console.WriteLine("Não forma um triangulo");
+            }
+        }
+    }
+}
