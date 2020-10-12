e1d28fe6a3f8bdc3a6905e3f02696379f9b1e285
Eduardo007-lang <Eduardo2018ads@hotmail.com>
2020-10-12 14:56:27 -0300

    Triangulo

---
+++ Calculo_Triangulo/Calculo_Triangulo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculo_Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis compostas
+            Triangulo x, y;
+            //Instanciando a classe triangulo
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Calcular area de X
+            double areaX = x.CalcularArea();
+
+            //Calcular area de Y
+            double areaY = y.CalcularArea();
+
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculo_Triangulo/Calculo_Triangulo/Triangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace Calculo_Triangulo
+{
+    class Triangulo
+    {
+        //Atributos da classe
+        public double A;
+        public double B;
+        public double C;
+
+        //Metodo da Classe
+        public double CalcularArea() {
+
+            double p = (A + B + C) / 2.0;
+
+            //double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C)); 
+           
+        }
+
+    }
+}
