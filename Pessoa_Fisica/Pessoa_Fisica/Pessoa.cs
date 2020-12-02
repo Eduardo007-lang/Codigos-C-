4a5d51dfcda53055698afe07e03ac819e2283b6c
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2020-12-02 13:03:48 -0300

    Concluido.

---
+++ Pessoa_Fisica/Pessoa_Fisica/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Pessoa_Fisica
+{
+    class Pessoa
+    {
+        //Atributos privados usando metodos get e set
+        public string Nome { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public double Altura { get; private set; }
+
+        //Construtor de 3 parametros 
+        public Pessoa(string nome, DateTime dataNascimento, double altura) {
+
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Altura = altura;
+        }
+
+        //Funcao para calcular a idade Datetime pegado data atual e diminuindo com o ano de nascimento armazenado no atributo DataNascimento
+        public int Idade() {
+
+            return DateTime.Today.Year - DataNascimento.Year;
+        }
+        //Funcao para transformar o objeto em string
+        public override string ToString()
+        {
+            return "Nome: " + Nome + ", Idade: " + Idade() + ", Altura: " + Altura.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Pessoa_Fisica/Pessoa_Fisica/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Pessoa_Fisica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instancia particular da classe
+            Pessoa p;
+
+            //Mostrar mensagem na tela do Usuário
+            Console.Write("Nome: ");
+            //variavel nome do tipo string
+            string nome = Console.ReadLine();
+            Console.Write("Data de Nascimento: ");
+            //variavel dataNascimento do tipo DataTime
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            //variavel altura do tipo double 
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Criando um objeto p com paramentros de entrada 
+            p = new Pessoa(nome, dataNascimento, altura);
+
+            //Saltar linha 
+            Console.WriteLine();
+            //Printando o valores do objeto p na tela do usuário 
+            Console.WriteLine(p);
+
+        }
+    }
+}
