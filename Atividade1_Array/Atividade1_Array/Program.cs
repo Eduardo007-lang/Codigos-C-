b66ef8153a761768baf737bb8e796a1274e536b6
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2020-12-15 13:14:42 -0300

    multiplicando e triblicando valores originais de uma Array.

---
+++ Atividade1_Array/Atividade1_Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Atividade1_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vect1 = new int[10];
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                vect1[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            int[] vect2 = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (vect1[i] % 2 == 0)
+                {
+
+                    vect2[i] = vect1[i] * 2;
+
+                }
+                else if (vect1[i] % 2 == 1)
+                {
+
+                    vect2[i] = vect1[i] * 3;
+                }
+
+                Console.WriteLine(vect2[i]);
+            }
+        }
+    }
+}
