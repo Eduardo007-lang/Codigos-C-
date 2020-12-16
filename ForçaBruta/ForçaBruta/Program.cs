070aabb0e0bd6ad34f4ea5b84740b250c00bca87
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2020-12-16 17:54:15 -0300

    Codigo forçaBruta

---
+++ ForçaBruta/ForçaBruta/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForçaBruta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numAleatorio = new Random();
+
+            int[] array = new int[9999];
+
+
+            for (int i = 0; i < array.Length; i++) {
+
+                int arrayInt = numAleatorio.Next(0, 9999);
+
+                for (int j = 0; j < array.Length; i++)
+
+                    if (array[i] == array[j]) {
+                        
+                    }
+
+                        array[i] = arrayInt;
+
+                        Console.WriteLine(array[i]);
+            }
+
+           // Console.WriteLine(array);
+        }
+    }
+}
