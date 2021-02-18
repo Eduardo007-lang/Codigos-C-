7fbfbfa1919e2cfce0d934c67dca8d842231e18c
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2021-02-18 12:41:18 -0300

    Teste

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Csharp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          double n1, n2, n3, media;
+
+          n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);  
+
+            media = (n1 * 2.0 * n2 * 3.0 * n3 * 5.0) / 10;
+
+        Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
