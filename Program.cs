062d17475fec3553b0f72fb457a85f945052c90b
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2021-02-18 13:46:17 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ namespace Csharp_test
     {
         static void Main(string[] args)
         {
-          double n1, n2, n3, media;
+           double n1, n2, n3, media;
 
-          n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-          n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-          n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);  
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            media = (n1 * 2.0 * n2 * 3.0 * n3 * 5.0) / 10;
+            media = ( n1 * 2 ) + ( n2 * 3 )+ ( n3 * 5) / 10.0;
 
-        Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+        Console.WriteLine("MEDIA = " + media);
 
         }
     }
